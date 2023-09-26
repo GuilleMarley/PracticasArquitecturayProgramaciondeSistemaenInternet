389991f821a93334fc7fe155b2e4b795937e2a87
GuilleMarley <guilleone177@gmail.com>
2023-09-26 18:51:14 +0200

    Añadida practica 1

--- Practica1/Ejercicio1.ts
+++ Practica1/Ejercicio1.ts
@@ -1,8 +1,22 @@
-//Hacer una función que tenga un parámetro de entrada que sea un array y en el cual se implemente un algoritmo de ordenación QuickSort. Criterios:
-  //  Debe ser recursivo
+const quickSort = (arr: number[]): number[] => {
+    if (arr.length <= 1) {
+        return arr
+    }
 
-const quickSort = ( arr: number[] ):number[] => {
-    const result: number[] = [];
-   
-    return result;
+    const pivot: number = arr[0]
+    const left: number[] = []
+    const right: number[] = []
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i])
+        } else {
+            right.push(arr[i])
+        }
+    }
+
+    left.push(pivot)
+    return quickSort(left).concat(quickSort(right))
 }
+
+console.log(quickSort([23, 44, 1, 32, 2, 4]))
