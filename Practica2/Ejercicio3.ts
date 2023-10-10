d446197cbda52a7d2908ae99ee4fe389741d8744
GuilleMarley <guilleone177@gmail.com>
2023-10-10 23:50:42 +0200

    practica2

--- Practica2/Ejercicio3.ts
+++ Practica2/Ejercicio3.ts
@@ -1,3 +1,4 @@
+//Creo el tipo libro con los parametros necesario y con el tipo de dato que son
 type Libro = {
     id: number
     title: string|null
@@ -6,18 +7,20 @@ type Libro = {
     genre: string|null
 }
 
+//Creo un array que va a ser mi biblioteca y un contador que va a servir para poner el id de los libros sin que se repitan
 const biblioteca: Libro[] = [];
 let counter = 0;
-
+//Creo un bucle a modo de menu
 while(1){
-    
+    //Le pido que quiere hacer al usuario
     const option = prompt("1.Create book.\n2.Filter by genre.\n3.Remove book.\n4.EXIT.\n");
-    
 
     if (option == null){
         console.log("NOT AVAILABLE OPTION SELECTED");
     }else{
-          
+        //La primera opcion es crear un libro en la biblioteca
+        //le pido al usuario los datos y creo un libro variable con esos datos 
+        //este libro luego lo añado al array de mi biblioteca y muestro los libros que hay
         if(parseInt(option) === 1){
 
             const title_var = prompt("Introduce el titulo del libro: ");
@@ -39,6 +42,7 @@ while(1){
 
             console.log("Actualmente hay estos libros:\n ", biblioteca)
 
+            //la opcion 2 es buscar por genero para lo que hago un filter y compruebo si coincide alguno de la biblioteca
        } else if(parseInt(option) === 2) {
 
            //biblioteca.filter((e)=>console.log(e))
@@ -46,6 +50,7 @@ while(1){
            const genreFound = biblioteca.filter((libro) => libro.genre === genreSearch)
            console.log("Hay estos libros con el genero ", genreSearch, "\n", genreFound)
 
+           //Aqui borraria libro pero no me funcionaba bien y prefiero que compile
        } else if(parseInt(option) === 3){
 
             console.log("Borro libro.\n")
